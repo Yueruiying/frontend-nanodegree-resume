88e39f41f035cb41e8f4a2603edf750378f95158
YueRuiying <1334993200@qq.com>
2017-05-22 14:26:16 +0800

    修改页面
    修改页面

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,10 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $('#main').append('YueRuiying');
+
+// var firstName = 'YueRuiying';
+ //var age = 25;
+ //console.log(firstName);
+
+ var awesomeThoughts = 'I am YueRuiying and I am AWESOME';
+ console.log(awesomeThoughts);
